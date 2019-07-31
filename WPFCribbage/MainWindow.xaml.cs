6f497c54706e8c00a02cc8cb3ab31df24b68363c
Brandon Cooper <46848839+brandon-2k@users.noreply.github.com>
2019-07-30 22:57:14 -0500

    Added a few example controls and images

--- WPFCribbage/MainWindow.xaml.cs
+++ WPFCribbage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,37 @@ namespace WPFCribbage
         public MainWindow()
         {
             InitializeComponent();
+            Person person = new Person("Brandon");
+            DataContext = person;
+        }
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChange("Name");
+            }
         }
     }
 }
