f68bb6ceebf2e6ea14f67c649196646914f00ee5
Brandon Cooper <46848839+brandon-2k@users.noreply.github.com>
2019-10-31 21:57:20 -0500

    started working on the game class

---
+++ WPFCribbage/CardType.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace WPFCribbage
+{
+    public class CardType
+    {
+        private Image CardFront { get; set; }
+        private Image CardBack { get; set; }
+
+        public CardType()
+        {
+            
+        }
+
+
+    }
+}
---
+++ WPFCribbage/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCribbage
+{
+    class Game
+    {
+        public Player Winner { get; set; }
+        public int NumDecks { get; set; }
+        public int NumPlayers { get; set; }
+        public CardType Deck { get; set; }
+        public bool IsDealer { get; set; }
+
+        public Game(int Decks, int Players, CardType Cards)
+        {
+            if (Decks < 1) // check for number of decks
+                throw new Exception("Less than one deck");
+
+            if (Players < 2) // check for number of players
+                throw new Exception("Less than two players");
+
+            if (Cards == null)
+                throw new Exception("Card type is null");
+            
+
+            NumDecks = Decks;
+            NumPlayers = Players;
+        }
+
+
+
+
+
+
+
+    }
+}
